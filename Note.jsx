815f3ab9ecd39da91afb7838852b63a60da9266b
Kelvin Patric Melvin <57454357+Kelvin-Patric-Melvin-2000@users.noreply.github.com>
2021-06-05 21:03:47 +0530

    Files added via upload

---
+++ Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazin bootcamp taken by Shaurya Sinha. The duration of the
+        bootcamp was 7 days. We were able to learn some basics of Javascript and
+        also about React.js . I wish to learn more about basic web development
+        and I will definitly work on that.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
